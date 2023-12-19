f25dbe6b9628e1a5fe085026e0892ce3bdcfe5f7
Caroline Dahmer <devcarolinedahmer@gmail.com>
2023-12-19 20:51:55 -0300

    Exercicio resolvido

---
+++ 02/src/index.ts
@@ -0,0 +1,49 @@
+const usuarios = [
+    {
+        nome: "Guido",
+        idade: 32,
+        status: true,
+    },
+    {
+        nome: "Dani",
+        idade: 30,
+        status: true,
+    },
+    {
+        nome: "João",
+        idade: 40,
+        status: false,
+    },
+    {
+        nome: "Guilherme",
+        idade: 29,
+        status: true,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+        status: false,
+    },
+    {
+        nome: "José",
+        idade: 28,
+        status: false,
+    },
+]
+
+const procurarUsuario = (usuarios : {
+    nome: string, 
+    idade: number, 
+    status: boolean} [], nome: string) : {
+        nome: string, 
+        idade: number, 
+        status: boolean }[] => {
+        const resultado = usuarios.filter((nomesLista) => {
+            return nomesLista.nome.toLowerCase().includes(nome.toLowerCase())
+        })
+
+        return resultado
+}
+
+
+console.log(procurarUsuario(usuarios,'jo'))
